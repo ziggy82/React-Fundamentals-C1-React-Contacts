e6c32c7421f14f49c7e5f6594a4d6f2e5b4ccb18
Zaheir Razak <zaheir786@hotmail.com>
2023-12-28 07:12:58 -0500

    adding state to the list contacts

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,37 @@
 import "../css/App.css";
+import ListContacts from "./ListContacts";
+import { useState } from "react";
 
 const App = () => {
-  return <div>Hello World</div>;
+  const removeContact = (contact) => {
+    setContacts(contacts.filter((c) => c.id !== contact.id));
+  };
+
+  const [contacts, setContacts] = useState([
+    {
+      id: "karen",
+      name: "Karen Isgrigg",
+      handle: "karen_isgrigg",
+      avatarURL: "http://localhost:5001/karen.jpg",
+    },
+    {
+      id: "richard",
+      name: "Richard Kalehoff",
+      handle: "richardkalehoff",
+      avatarURL: "http://localhost:5001/richard.jpg",
+    },
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "tylermcginnis",
+      avatarURL: "http://localhost:5001/tyler.jpg",
+    },
+  ]);
+  return (
+    <div>
+      <ListContacts onDeleteContact={removeContact} contacts={contacts} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,9 +1,28 @@
-const ListContacts = () => {
-    return (
-        <ol>
-            
-        </ol>
-    )
-}
+const ListContacts = ({ contacts, onDeleteContact }) => {
+  return (
+    <ol className="contact-list">
+      {contacts.map((contact) => (
+        <li key={contact.id} className="contact-list-item">
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`,
+            }}
+          ></div>
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button
+            onClick={() => onDeleteContact(contact)}
+            className="contact-remove"
+          >
+            Remove
+          </button>
+        </li>
+      ))}
+    </ol>
+  );
+};
 
 export default ListContacts;
